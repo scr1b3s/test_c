7aaeeaeabacdd010818c7b55a9cda083edccf7fd
André Ferreira <andrde-s@student.42.rio>
2024-10-15 21:36:46 -0300

    Changing Ft_putchar Tests

--- tests/test_ft_putchar.c
+++ tests/test_ft_putchar.c
@@ -1,10 +1,7 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
-
-void	ft_putchar(char c);
+#include "../src/ft_putchar.c"
 
 // Helper function to capture stdout
 void redirect_stdout(void)
@@ -15,25 +12,27 @@ void redirect_stdout(void)
 Test(ft_putchar, test_newline, .init = redirect_stdout)
 {
     ft_putchar('\n');
-    cr_assert_stdout_eq_str("\n", "Expected '\\n' but got different output");
+    const char *output = cr_get_redirected_stdout();
+    cr_assert_str_eq(output, "\n", "Expected '\\n' but got '%s'", output);
 }
 
 Test(ft_putchar, test_tab, .init = redirect_stdout)
 {
     ft_putchar('\t');
-    cr_assert_stdout_eq_str("\t", "Expected '\\t' but got different output");
+    const char *output = cr_get_redirected_stdout();
+    cr_assert_str_eq(output, "\t", "Expected '\\t' but got '%s'", output);
 }
 
 Test(ft_putchar, test_null, .init = redirect_stdout)
 {
     ft_putchar('\0');
-    cr_assert_stdout_eq_str("\0", "Expected '\\0' but got different output");
+    const char *output = cr_get_redirected_stdout();
+    cr_assert_str_eq(output, "\0", "Expected '\\0' but got '%s'", output);
 }
 
 Test(ft_putchar, test_ff, .init = redirect_stdout)
 {
     ft_putchar('\xFF');
-    cr_assert_stdout_eq_str("\xFF", "Expected '\\xFF' but got different output");
+    const char *output = cr_get_redirected_stdout();
+    cr_assert_str_eq(output, "\xFF", "Expected '\\xFF' but got '%s'", output);
 }
-
-// Add more test cases here as needed
